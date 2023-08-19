232597535258bd18094262367435fed4bedee324
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-08-19 16:14:31 -0300

    feat: articles displayed on the page

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,23 +1,49 @@
 // import { useState, useEffect } from "react"
 
-import { useState } from "react";
-import { linkIcon } from "../assets";
-
+import { useEffect, useState } from "react";
+import { copy, linkIcon } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 const Demo = () => {
-  const [article, setArticle ] = useState({
+  const [article, setArticle] = useState({
     url: "",
-    summary: ""
-  })
+    summary: "",
+  });
+
+  const [allArticles, setAllArticles] = useState([]);
+  const [getSummary] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles"),
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
-    alert("Submitted")
-  }
+    e.preventDefault();
 
- 
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setAllArticles(updatedAllArticles);
+
+      setArticle(newArticle);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-x1">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
           <img
             src={linkIcon}
             alt="link_icon"
@@ -27,28 +53,44 @@ const Demo = () => {
             type="url"
             placeholder="Coleque sua URL"
             value={article.url}
-            onChange={(e) => setArticle({
-              ...article, url: e.target.value
-            })
-          
-          }
-            
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
             required
             className="url_input peer"
           />
-          <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
-          ↵
-          
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
           </button>
         </form>
-
         {/* Browser URL History */}
-
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
-
       {/* Display results */}
     </section>
   );
+  
 };
 
 export default Demo;
