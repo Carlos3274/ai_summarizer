687d29d7fe4606deee7f0610d5da4a7dea66dc1d
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-08-19 19:36:47 -0300

    refactor: imports fixed

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
+
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
