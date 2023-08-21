e578764cacc53ad638fa849a7a410962ed94f72a
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-08-21 12:46:41 -0300

    chore: endpoint changed to my api

--- src/services/article.js
+++ src/services/article.js
@@ -1,29 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
-
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
-    prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", rapidApiKey),
-        headers.set(
-          "X-RapidAPI-Host",
-          "article-extractor-and-summarizer.p.rapidapi.com",
-        );
-
-      return headers;
-    },
+    baseUrl: "https://summize.onrender.com",
   }),
   endpoints: (builder) => ({
     getSummary: builder.query({
       query: (params) =>
-        `/summarize?url=${encodeURIComponent(
-          params.articleUrl,
-        )}&length=3&lang=pt`,
+        `/summary?url=${encodeURIComponent(params.articleUrl)}`,
     }),
   }),
 });
 
 export const { useLazyGetSummaryQuery } = articleApi;
+
+export default articleApi;
